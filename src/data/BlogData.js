e1212e3ae7498e0605db4ebb7f3b940a6a1540fe
Greg Thomas Andes <58809832+gtandes@users.noreply.github.com>
2022-11-21 16:41:06 +0800

    Add files via upload

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -3,7 +3,7 @@ export const Blogs = [
 		id: 1,
 		name: 'Ethereum Course Marketplace in Ropsten',
 		tags: ['nextjs', 'solidity', 'tailwind'],
-		date: 'September 2022',
+		date: 'December 2021',
 		imgSrc: 'https://i.postimg.cc/gJ6wx9jv/web3-1.png',
 		link: 'https://eth-coursemarketplace.vercel.app/marketplace',
 	},
@@ -11,47 +11,49 @@ export const Blogs = [
 	{
 		id: 2,
 		name: 'Krypt: Your Modern Crypto Paypal ',
-		tags: ['nextTS', 'solidity', 'tailwindcss', 'hardhat', 'web3'],
-		date: 'October 2022',
+		tags: ['reactJS', 'solidity', 'tailwindcss', 'hardhat', 'web3'],
+		date: 'March 2022',
 		imgSrc: 'https://i.postimg.cc/QC0kYxbL/web3-2.png',
-		link: 'https://eth-coursemarketplace.vercel.app/marketplace',
+		link: 'https://krypt-goerli-giphy.netlify.app/',
 	},
 
 	{
 		id: 3,
-		name: 'Lottery',
-		tags: ['nextTS', 'solidity', 'tailwindcss', 'open-zeppelin', 'thirdweb'],
-		date: '6 July, 2021',
+		name: 'Uniswap 2.0',
+		tags: ['reactJS', 'solidity', 'hardhat', 'web3', 'cranq', 'tailwindcss'],
+		date: 'May 2022',
 		imgSrc:
-			'https://i.postimg.cc/mDfZrrXQ/Screenshot-from-2022-10-05-17-50-23.png',
-		link: 'https://eth-coursemarketplace.vercel.app/marketplace',
+			'https://i.postimg.cc/VsCMw5s9/Screenshot-from-2022-10-24-04-53-14.png',
+		link: 'https://uniswapv2-usedapp-cranq.netlify.app/',
 	},
 
 	{
 		id: 4,
-		name: 'NFT Drop App',
-		tags: ['nextTS', 'solidity', 'tailwindcss', 'sanity', 'thirdweb'],
-		date: '26 May, 2021',
+		name: 'NFT Marketplace Landing Page',
+		tags: ['react native', 'tailwindcss', 'expo go'],
+		date: 'October 2022',
 		imgSrc:
-			'https://i.postimg.cc/x8Jjtb4N/Screenshot-from-2022-10-05-18-09-01.png',
-		link: 'https://eth-coursemarketplace.vercel.app/marketplace',
+			'https://i.postimg.cc/zXqY4qLK/Screenshot-from-2022-10-26-08-10-39.png',
+		link: 'https://nft-marketplace-landingpage-swart.vercel.app/',
 	},
 
 	{
 		id: 5,
-		name: 'Under construction',
-		tags: ['tbd', 'tbd', 'tbd'],
-		date: 'tbd',
-		imgSrc: '',
-		link: '',
+		name: 'NFT Drop Marketplace',
+		tags: ['thirdweb', 'nextTS', 'sanityIO', 'tailwindcss', 'ethers'],
+		date: 'October 2022',
+		imgSrc:
+			'https://i.postimg.cc/fTBj5f7q/Screenshot-from-2022-11-18-21-50-33.png',
+		link: 'https://gta-nft-drop.vercel.app/',
 	},
 
 	{
 		id: 6,
-		name: 'Under construction',
-		tags: ['tbd', 'tbd', 'tbd'],
-		date: 'tbd',
-		imgSrc: '',
-		link: '',
+		name: 'AvaxGods NFT CardBattle',
+		tags: ['solidity', 'reactJS', 'ethers', 'tailwindcss'],
+		date: 'November 2022',
+		imgSrc:
+			'https://i.postimg.cc/W1qdXTsh/Screenshot-from-2022-11-18-17-22-03.png',
+		link: 'https://gta-nft-cardbattle.vercel.app/',
 	},
 ];
